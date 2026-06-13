2f7e88a8b81136a6d17b93f7687ff9fef9671ae2
butterbeerrr <ranisa@student.telkomuniversity.ac.id>
2026-06-14 00:46:52 +0800

    register course and category api routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CourseCategoryController;
+use App\Http\Controllers\Api\CourseController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::apiResource('categories', CourseCategoryController::class);
+Route::apiResource('courses', CourseController::class);
